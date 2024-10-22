306d3afd122ff6f1e2a80578701f546897564e6e
Osman Yetkin <155627715+osmanveyetkin@users.noreply.github.com>
2024-10-22 15:24:00 +0300

    Add files via upload

---
+++ csödev/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random randomMaker = new Random();
+        int number = randomMaker.Next(1, 10001);
+        Console.WriteLine("Rastgele oluşturulan sayı:" + number);
+        int sum = 0;
+        while (number > 0)
+        {
+            int digit = number % 10;
+            sum += digit;
+            number = number / 10;
+        }
+        if (sum % 2 == 0)
+        {
+            Console.WriteLine("Rakamları toplamı çifttir.");
+        }
+        else
+        {
+            Console.WriteLine("Rakamları toplamı tektir.");
+        }
+    }
+}
